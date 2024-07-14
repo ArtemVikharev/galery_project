0b6b30dae6afd0e0bf39dc00a527b95a25685f46
ArtemVikharev <webavikharev@gmail.com>
2024-07-14 18:23:40 +0300

    complete feature/db_connect

---
+++ vendor/DB.php
@@ -0,0 +1,35 @@
+<?php 
+    require_once "config.php";
+
+    class DB{ 
+        public $connect = null;
+
+        public function __construct(){
+                $this->connect();
+        }
+
+
+        public function connect(){
+            $this->connect = new mysqli(HOST, USER, PASSWORD, DB_NAME);
+        }
+        public function query($sql, $responseData = true){
+            $result = $this->connect->query($sql);
+
+            if (!$result){
+                die("error");
+            }
+            
+            if($responseData){
+                $data = [];
+
+                while($row = $result->fetch_array(MYSQLI_ASSOC)){
+                    $data[] = $row;
+                }   
+            
+            }
+
+            return $data;
+
+        }
+
+    }

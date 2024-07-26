72823c908bdbf0f8a553117dc5f20477c69cdd3a
ArtemVikharev <webavikharev@gmail.com>
2024-07-26 11:54:53 +0300

    complete feature/validation

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -2,18 +2,15 @@
 
 require_once "Model/User.php";
 require_once "View/View.php";
+require_once "Helper/Validator.php";
 
 
 class MainController{
 
     public function indexAction(){
-        // echo "Главная страница";
+
         $data = [];
 
-        // if (!$data || count($data) == 0) {
-
-        //     header("location: http://" . $_SERVER["SERVER_NAME"] . "" . $_SERVER["SCRIPT_NAME"] . "?route=main/error");
-        // }
         View::render(["page_view" => "default", "layout" => "default", $data]);
     }
 
@@ -25,12 +22,18 @@ class MainController{
     }
 
     public function registerformAction(){
-        $newUser = new User($_POST['user_firstname'], $_POST['user_surname'],$_POST['user_username'],$_POST['user_email'],$_POST['user_password']);
-        $newUser->registerUser();
-
-        header("location: http://" . $_SERVER["SERVER_NAME"] . "" . $_SERVER["SCRIPT_NAME"] . "?route=main/index");
+        $registerData = $_POST;
+        $errors = Validator::isValid($registerData);
+        if(!$errors){
+            header("location: ?route=main/register&status=".json_encode([$errors, $registerData]));
+        }
+        else{
+            echo "регистрация";
+            $newUser = new User($_POST['user_firstname'], $_POST['user_surname'],$_POST['user_username'],$_POST['user_email'],$_POST['user_password']);
+            $newUser->registerUser();
+            header("location: http://" . $_SERVER["SERVER_NAME"] . "" . $_SERVER["SCRIPT_NAME"] . "?route=main/index");
+        }         
     }
-
 
     public function errorAction(){
         echo "Ошибка открытия страницы";

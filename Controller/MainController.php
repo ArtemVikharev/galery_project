73d8af09c4123fef6b9e5ffc2da049a17feb90b4
ArtemVikharev <webavikharev@gmail.com>
2024-07-28 14:26:32 +0300

    complete bug_fix/registration

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -24,15 +24,13 @@ class MainController{
     public function registerformAction(){
         $registerData = $_POST;
         $errors = Validator::isValid($registerData);
-
-        if($errors){
+        if(!empty(array_diff($errors, array('')))){
             header("location: ?route=main/register&status=".json_encode([$errors, $registerData]));
+            die();
         }
-        else{
-            $newUser = new User($_POST['user_firstname'], $_POST['user_surname'],$_POST['user_username'],$_POST['user_email'],$_POST['user_password']);
-            $newUser->registerUser();
-            header("location: http://" . $_SERVER["SERVER_NAME"] . "" . $_SERVER["SCRIPT_NAME"] . "?route=main/index");
-        }         
+        $newUser = new User($_POST['user_firstname'], $_POST['user_surname'],$_POST['user_username'],$_POST['user_email'],$_POST['user_password']);
+        $newUser->registerUser();
+        header("location: http://" . $_SERVER["SERVER_NAME"] . "" . $_SERVER["SCRIPT_NAME"] . "?route=main/index");   
     }
     
     public function addimageAction(){
